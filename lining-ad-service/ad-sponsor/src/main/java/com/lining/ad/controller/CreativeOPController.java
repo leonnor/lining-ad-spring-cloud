858469389fce43f836961c95514aad7ac8891a6a
leonnor <958978379@qq.com>
2019-03-22 18:57:43 +0800

    controller

--- lining-ad-service/ad-sponsor/src/main/java/com/lining/ad/controller/CreativeOPController.java
+++ lining-ad-service/ad-sponsor/src/main/java/com/lining/ad/controller/CreativeOPController.java
@@ -8,6 +8,7 @@ import com.lining.ad.vo.CreativeRequest;
 import com.lining.ad.vo.CreativeResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +31,7 @@ public class CreativeOPController {
         this.creativeService = creativeService;
     }
 
+    @PostMapping("/create/creative")
     public CreativeResponse createCreative(@RequestBody CreativeRequest request) throws AdException {
         log.info("ad-sponsor: createCreative -> {}", JSON.toJSONString(request));
         return creativeService.createCreative(request);
