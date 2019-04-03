fdca737b2d77536c9780329221616713ea42d419
leonnor <958978379@qq.com>
2019-04-03 20:36:08 +0800

    binlog

---
+++ lining-ad-service/ad-search/src/main/java/com/lining/ad/service/BinlogServiceTest.java
@@ -0,0 +1,45 @@
+package com.lining.ad.service;
+
+import com.github.shyiko.mysql.binlog.BinaryLogClient;
+import com.github.shyiko.mysql.binlog.event.DeleteRowsEventData;
+import com.github.shyiko.mysql.binlog.event.EventData;
+import com.github.shyiko.mysql.binlog.event.UpdateRowsEventData;
+import com.github.shyiko.mysql.binlog.event.WriteRowsEventData;
+
+/**
+ * className BinlogServiceTest
+ * description TODO
+ *
+ * @author ln
+ * @version 1.0
+ * @date 2019/4/3 19:55
+ */
+public class BinlogServiceTest {
+
+    public static void main(String[] args) throws Exception {
+
+        BinaryLogClient client = new BinaryLogClient(
+                "127.0.0.1",
+                3306,
+                "root",
+                "958978379"
+        );
+        client.registerEventListener(event -> {
+
+            EventData data = event.getData();
+
+            if (data instanceof UpdateRowsEventData){
+                System.out.println("Update----------------");
+                System.out.println(data.toString());
+            } else if (data instanceof WriteRowsEventData){
+                System.out.println("Write----------------");
+                System.out.println(data.toString());
+            } else if (data instanceof DeleteRowsEventData){
+                System.out.println("Delete----------------");
+                System.out.println(data.toString());
+            }
+        });
+
+        client.connect();
+    }
+}
